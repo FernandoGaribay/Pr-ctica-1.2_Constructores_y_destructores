35b0efa799c2d6ea7cbebf2930f99b000f8a9907
Fernando Garibay <garibaycejaf@gmail.com>
2022-02-22 10:34:11 -0600

    Se agregaron los comentarios Se renombro el metodo verDatosCuenta() Se declaran los destructores

--- Clase Cuenta/Clase Cuenta.cpp
+++ Clase Cuenta/Clase Cuenta.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <cstring>
-#pragma warning(disable : 4996)
+#pragma warning(disable : 4996) // Desabilita las warning's de metodos como strcpy_s
 
 using namespace std;
 
-class Cuenta {
+class Cuenta { // Clase Cuenta
 private:
-    int numeroCuenta;
-    char* nombreTitular;
-    double saldo;
+    int numeroCuenta; // Atributo: Numero de cuenta
+    char* nombreTitular; // Atributo: Nombre del titular
+    double saldo; // Atributo: Saldo
 
 public:
     Cuenta();
@@ -16,17 +16,17 @@ public:
     Cuenta(const Cuenta&);
     ~Cuenta();
 
-    void setNumeroCuenta(int);
-    void setNombreTitular(char const*);
-    void setSaldo(double);
+    void setNumeroCuenta(int); // Set: Numero de cuenta
+    void setNombreTitular(char const*); // Set: Nombre del titular
+    void setSaldo(double); // Set: Saldo
 
-    int getNumeroCuenta();
-    char* getNombreTitular();
-    double getSaldo();
+    int getNumeroCuenta(); // Get: Numero de cuenta
+    char* getNombreTitular(); // Get: Nombre del titular
+    double getSaldo(); // Get: Saldo
 
     void depositar(double);
     void retirar(double);
-    void verDatosCuenta();
+    void mostrarCuenta(); // Metodo: Mostrar cuenta
 };
 
 // Constructor por defecto
@@ -58,28 +58,28 @@ Cuenta::~Cuenta() {
     delete[] nombreTitular;
 }
 
-void Cuenta::setNumeroCuenta(int numeroCuenta) {
+void Cuenta::setNumeroCuenta(int numeroCuenta) { // Set: Numero de cuenta
     this->numeroCuenta = numeroCuenta;
 }
 
-void Cuenta::setNombreTitular(char const* nombreTitular) {
+void Cuenta::setNombreTitular(char const* nombreTitular) { // Set: Nombre del titular
     strcpy(this->nombreTitular, nombreTitular);
 }
 
-void Cuenta::setSaldo(double saldo) {
+void Cuenta::setSaldo(double saldo) { // Set: Saldo
     this->saldo = saldo;
 }
 
 
-int Cuenta::getNumeroCuenta() {
+int Cuenta::getNumeroCuenta() { // Get: Numero de cuenta
     return numeroCuenta;
 }
 
-char* Cuenta::getNombreTitular() {
+char* Cuenta::getNombreTitular() { // Get: Nombre del titular
     return nombreTitular;
 }
 
-double Cuenta::getSaldo() {
+double Cuenta::getSaldo() { // Get: Saldo
     return saldo;
 }
 
@@ -89,11 +89,10 @@ void Cuenta::depositar(double cantidad) {
 }
 
 void Cuenta::retirar(double cantidad) {
-    if (saldo > cantidad)
-        this->saldo -= cantidad;
+    this->saldo -= cantidad;
 }
 
-void Cuenta::verDatosCuenta() {
+void Cuenta::mostrarCuenta() { // Metodo: Mostrar cuenta
     cout << "\nNumero de cuenta: " << getNumeroCuenta()
         << "\nNombre del titular: " << getNombreTitular() 
         << "\nSaldo: " << getSaldo() 
@@ -102,16 +101,20 @@ void Cuenta::verDatosCuenta() {
 
 int main()
 {
-    Cuenta cuenta1;
-    Cuenta cuenta2(2, "Fernando", 1000);
-    Cuenta cuenta3 = cuenta2;
+    Cuenta cuenta1; // Objeto de Cuenta1
+    Cuenta cuenta2(2, "Fernando", 1000); // Objeto de Cuenta2 (Con los parametros que pide el constructor)
+    Cuenta cuenta3 = cuenta2; // Objeto de Cuenta3 (Copia del objeto cuenta2)
 
-    cuenta3.depositar(200);
-    cuenta3.retirar(100);
+    cuenta3.depositar(200); // Se deposita 200$ pesos a la cuenta 3
+    cuenta3.retirar(100); // Se retira 100$ pesos en la cuenta 2
 
-    cuenta1.verDatosCuenta();
-    cuenta2.verDatosCuenta();
-    cuenta3.verDatosCuenta();
+    cuenta1.mostrarCuenta(); // Se muestra la informacion del objeto cuenta1
+    cuenta2.mostrarCuenta(); // Se muestra la informacion del objeto cuenta2
+    cuenta3.mostrarCuenta(); // Se muestra la informacion del objeto cuenta3
+
+    cuenta1.~Cuenta(); // Se destruye el objeto cuenta1
+    cuenta2.~Cuenta(); // Se destruye el objeto cuenta2
+    cuenta3.~Cuenta(); // Se destruye el objeto cuenta3
 
     cout << endl;
     system("pause");
